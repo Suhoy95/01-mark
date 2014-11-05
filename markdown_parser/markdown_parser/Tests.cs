667d2c9c30e29e212f42cbe1aa9a43123f0c034e
Suhoy95 <suhoy95@gmail.com>
2014-11-05 16:27:35 +0400

    start project

---
+++ markdown_parser/markdown_parser/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace markdown_parser
+{
+    [TestFixture]
+    class Parser_should
+    {
+        [Test]
+        public static void return_simple_input_string()
+        {
+            Parser a = new Parser();
+            Assert.AreEqual("Hello", a.toParse("Hello"));
+        }
+
+        [Test]
+        public static void replace_underline_tag()
+        {
+            Parser a = new Parser();
+            Assert.AreEqual("<em>Hello</em>", a.toParse("_Hello_"));
+        }
+
+        [Test]
+        public static void noreplace_one_underline()
+        {
+            Parser a = new Parser();
+            Assert.AreEqual("_Hello", a.toParse("_Hello"));
+        }
+    }
+}
---
+++ markdown_parser/markdown_parser/parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace markdown_parser
+{ 
+    class Parser
+    { 
+
+        public string toParse(string input)
+        {
+            input = ' ' + input + ' ';
+            string output = "";
+            string replacing = "";
+            for (var i = 1; i < input.Length-1; i++)
+            {
+                replacing = "";
+                replacing += tryReplaceTwiceUnderline(input, i);
+                replacing += tryReplaceOnesUnderline(input, i);
+                replacing += tryReplaceBackticks(input, i);
+                if (string.IsNullOrEmpty(replacing))
+                    output += input[i];
+                else
+                    output += replacing;
+            }
+          
+            return output;
+        }
+
+        private string tryReplaceBackticks(string input, int i)
+        {
+            if (input[i] != '`')
+                return "";
+            return "<code>";
+        }
+
+        private string tryReplaceOnesUnderline(string input, int i)
+        {
+            if (string.IsNullOrWhiteSpace(input[i-1].ToString()) && input[i] == '_')
+                for(var j = i+1; j < input.Length-1; j ++)
+                    if(tryReplaceOnesUnderline(input, j) == "</em>")
+                        return "<em>";
+
+            if(input[i-1] != '_' && input[i] =='_' &&
+                String.IsNullOrWhiteSpace(input[i+1].ToString()))
+                return "</em>";
+            
+            return "";
+        }
+
+        private string tryReplaceTwiceUnderline(string input, int i)
+        {
+            return "";
+        }
+    }
+}
